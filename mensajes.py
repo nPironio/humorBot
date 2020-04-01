daa0a6e32c82df16e43308b245b365f6c027ca97
NicoPironio <nicopironio@gmail.com>
2020-04-01 17:24:00 -0300

    Agrego mensaje de borrarTodo

--- mensajes.py
+++ mensajes.py
@@ -3,6 +3,7 @@
 commandList=[('/start', 'Te da la bienvenida'), 
 			 ('/resumen', 'Para cada humor, cuantas veces lo sentiste'),
 			 ('/resumenSemanal', 'Te muestra esta semana como estuvieron tus humores'),
-			 ('/borrar <humor>', 'Borra el humor de tus datos')]
+			 ('/borrar <humor1> <humor2> ...', 'Borra el/los humor/es de tus datos'),
+			 ('/borrarTodo', 'Borra todos tus datos')]
 
 startMessage = "Hola! Soy el humorBot \n\nMandame mensajes con tu humor del momento y lo guardo. \nDespues podes usar mis comandos para resumenes de lo que me fuiste mandando. \nSi no sabes mis comandos escribi /comandos !"
