e8b52905e5f344cfbe7e5fa341d4a0435fa38412
NicoPironio <nicopironio@gmail.com>
2020-04-01 19:53:01 -0300

    saco archivo repetido

--- comm_info.py
+++
@@ -1,6 +0,0 @@
-def start(update, context):
-	context.bot.send_message(chat_id=update.effective_chat.id, text=startMessage)
-
-def comandos(update, context):
-msg = "\n".join([comm+": "+desc for comm, desc in commandList])
-context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
