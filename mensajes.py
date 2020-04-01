233b6af35847142ddd7ca43817deed005bfa6baa
NicoPironio <nicopironio@gmail.com>
2020-04-01 19:54:55 -0300

    Agrego mensaje de borrarRecientes

--- mensajes.py
+++ mensajes.py
@@ -4,6 +4,7 @@ commandList=[('/start', 'Te da la bienvenida'),
 			 ('/resumen', 'Para cada humor, cuantas veces lo sentiste'),
 			 ('/resumenSemanal', 'Te muestra esta semana como estuvieron tus humores'),
 			 ('/borrar <humor1> <humor2> ...', 'Borra el/los humor/es de tus datos'),
+			 ('/borrarRecientes x <humor>', "Borra los 'x' guardados mas recientes de 'humor'")
 			 ('/borrarTodo', 'Borra todos tus datos')]
 
 startMessage = "Hola! Soy el humorBot \n\nMandame mensajes con tu humor del momento y lo guardo. \nDespues podes usar mis comandos para resumenes de lo que me fuiste mandando. \nSi no sabes mis comandos escribi /comandos !"
